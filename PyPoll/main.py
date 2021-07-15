1f3e10ac2940e11ad9c62e18626ac6237a57c7c0
sshaughnessy <s.shaughnessy.311@gmail.com>
2021-07-14 23:40:56 -0500

    update all files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,5 @@
 #import libraries
+from collections import namedtuple
 import csv
 import os
 
@@ -6,13 +7,14 @@ import os
 poll_csv = os.path.join("Resources", "election_data.csv")
 
 #output path
-#output = os.path.join("Analysis", "pypoll_ouput.txt")
+output = os.path.join("Analysis", "pypoll_ouput.txt")
 
 #define some variables
 
 candidate_percent = {}
 candidate_votes = {}
 total_votes = 0
+winner = 0
 
 #read in csv 
 with open(poll_csv, "r") as csvfile:
@@ -21,7 +23,6 @@ with open(poll_csv, "r") as csvfile:
 #loop through and count total votes + create dictionary of candidates w/ votes
     for rows in csvreader:
         total_votes += 1
-        
         if rows[2] in candidate_votes:
             candidate_votes[rows[2]] +=1
         else:
@@ -32,12 +33,22 @@ with open(poll_csv, "r") as csvfile:
         candidate_percent = (candidate_votes[candidate]/total_votes)*100
 
         
+winner = max(candidate_votes, key=candidate_votes.get)
 
 
-
+poll_output = ( f"Election Results\n"
+                f"------------------------\n"
+                f"Total votes: {total_votes}\n"
+                f"------------------------\n"
+                f"Votes by Candidate: {candidate_votes}\n"
+                f"------------------------\n"
+                f"Candidate Vote Share: {candidate_percent}%\n"
+                f"------------------------\n"
+                f"Winner: {winner}\n")
 
         
-        
-
+print(poll_output)
     
-    
+#export output to .txt file
+with open(output,'w') as textfile:
+    textfile.write(poll_output)  
