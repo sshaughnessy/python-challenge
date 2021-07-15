d0ac8768e634b65b38fb48b951a139a5348c1e33
sshaughnessy <s.shaughnessy.311@gmail.com>
2021-07-15 15:12:43 -0500

    update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,45 +10,56 @@ poll_csv = os.path.join("Resources", "election_data.csv")
 output = os.path.join("Analysis", "pypoll_ouput.txt")
 
 #define some variables
-
-candidate_percent = {}
-candidate_votes = {}
+candidates = []
+candidate_percent = []
+candidate_votes = []
 total_votes = 0
-winner = 0
 
 #read in csv 
 with open(poll_csv, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csvheader = next(csvreader)
-#loop through and count total votes + create dictionary of candidates w/ votes
+#loop through and count total votes + create list of candidates + add a vote for each occurrence of candidate
     for rows in csvreader:
         total_votes += 1
-        if rows[2] in candidate_votes:
-            candidate_votes[rows[2]] +=1
+        if rows[2] not in candidates:
+            candidates.append(rows[2])
+            index = candidates.index(rows[2])
+            candidate_votes.append(1)
         else:
-            candidate_votes = 1
+            index = candidates.index(rows[2])
+            candidate_votes[index] +=1
 
-#loop through candidate directory to find vote %
-    for candidate in candidate_votes:
-        candidate_percent = (candidate_votes[candidate]/total_votes)*100
-
-        
-winner = max(candidate_votes, key=candidate_votes.get)
-
-
-poll_output = ( f"Election Results\n"
-                f"------------------------\n"
-                f"Total votes: {total_votes}\n"
-                f"------------------------\n"
-                f"Votes by Candidate: {candidate_votes}\n"
-                f"------------------------\n"
-                f"Candidate Vote Share: {candidate_percent}%\n"
-                f"------------------------\n"
-                f"Winner: {winner}\n")
+#loop through candidate votes list to caluclate vote %
+    for votes in candidate_votes:
+        vote_share = (votes/total_votes)*100
+        vote_share = round(vote_share)
+        vote_share = "%.3f%%" % vote_share
+        candidate_percent.append(vote_share)
 
         
-print(poll_output)
+winner = max(candidate_votes)
+index = candidate_votes.index(winner)
+elected = candidates[index]
+
+print(f"Election Results\n")
+print(f"------------------------\n")
+print(f"Total votes: {total_votes}\n")
+print(f"------------------------\n")
+for i in range(len(candidates)):
+    print(f"{candidates[i]} : {str(candidate_percent[i])} ({str(candidate_votes[i])})")
+print(f"------------------------\n")
+print(f"Winner: {elected}\n")
+
+        
     
 #export output to .txt file
 with open(output,'w') as textfile:
-    textfile.write(poll_output)  
+    textfile.write(f"Election Results\n")  
+    textfile.write(f"------------------------\n")
+    textfile.write(f"Total votes: {total_votes}\n")
+    textfile.write(f"------------------------\n")
+    for i in range(len(candidates)):
+        textfile.write(f"{candidates[i]} : {str(candidate_percent[i])} ({str(candidate_votes[i])})\n")
+    textfile.write(f"------------------------\n")
+    textfile.write(f"Winner: {elected}")
